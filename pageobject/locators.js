e642db9e953d50590a3ee50fdc9e051e5697f082
Sumit Mishra <sumit.mishra@iimmpact.com>
2024-07-23 18:21:20 +0530

    assertions

--- pageobject/locators.js
+++ pageobject/locators.js
@@ -10,7 +10,7 @@ class PageObject {
     this.newApplicationLink = '#page-inner-content > section.page-section.pt-2 > div > div > div.my-3 > a';
     this.emailInput = 'input[type="email"]';
     this.passwordInput = 'input[type="password"]';
-    this.textSource = '#saltcorn-builder > div.row > div.col-sm-auto.left-builder-col.builder-left-shrunk > div.componets-and-library-accordion.toolbox-card > div.card.mt-1 > div:nth-child(1) > div:nth-child(1) > div > svg';
+    this.textSource = '#saltcorn-builder > div.row > div.col-sm-auto.left-builder-col.builder-left-shrunk > div.componets-and-library-accordion.toolbox-card > div.card.mt-1 > div:nth-child(1) > div:nth-child(1)';
     this.textlocator='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > div.border > div';
     this.target = '#builder-main-canvas > div > div > div';
     this.lineBreakSource = '#saltcorn-builder > div.row > div.col-sm-auto.left-builder-col.builder-left-shrunk > div.componets-and-library-accordion.toolbox-card > div.card.mt-1 > div:nth-child(2) > div:nth-child(1) > div';
@@ -21,7 +21,7 @@ class PageObject {
     this.saveButton = '#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > button';
     this.newPageButton = '#page-inner-content > div:nth-child(2) > div.card.shadow.mt-0 > div > div > a';
     this.newPage_sidebar='#accordionSidebar > li:nth-child(6) > a > span';
-    this.newPageNameInput = 'input[type="text"]';
+    this.newPageNameInput = '#inputname';
     this.settingsTab = '#accordionSidebar > li:nth-child(7) > a > span';
     this.aboutApplicationLink = '#collapseSettings > div > a:nth-child(1)';
     this.systemSettingsLink = '#page-inner-content > div:nth-child(2) > div.card.shadow.mt-0 > div > div > ul > li:nth-child(4) > a';
@@ -32,6 +32,26 @@ class PageObject {
     this.linklocator = '#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table.w-100 > tbody > tr:nth-child(1) > td:nth-child(2) > input';
     this.linkurllocator = '#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table.w-100 > tbody > tr:nth-child(3) > td:nth-child(2) > input';
     this.expectedtoastermsg = 'Deleted all tables, views, pages, page_groups, files, triggers, eventlog, library, config, plugins';
+    this.deletebutton='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > button.btn.btn-sm.btn-danger';
+    this.clonebutton='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > button.btn.btn-sm.btn-secondary.ms-2'; 
+    this.CardUrl='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > table > tbody > tr:nth-child(2) > td > input';
+    this.SearchLocator='#saltcorn-builder > div.row > div.col-sm-auto.left-builder-col.builder-left-shrunk > div.componets-and-library-accordion.toolbox-card > div.card.mt-1 > div:nth-child(5) > div:nth-child(1)';  
+    this.Autofocuscheckbox='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > div:nth-child(3) > input';
+    this.statebadgecheckbox='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > div:nth-child(2) > input';
+    this.hasdropdowncheckbox='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > div:nth-child(1) > input';
+    this.containsdraglocator='#saltcorn-builder > div.row > div.col-sm-auto.left-builder-col.builder-left-shrunk > div.componets-and-library-accordion.toolbox-card > div.card.mt-1 > div:nth-child(6) > div:nth-child(1)';
+    this.containerdisplaysetting='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div:nth-child(6)';
+    this.containercontentsetting='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div:nth-child(7)';
+    this.containerflexsetting='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div:nth-child(8)';
+    this.containercontentlink='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div:nth-child(10)';
+    this.containercustomclass='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div:nth-child(11)';
+    this.ActionLocator='#saltcorn-builder > div.row > div.col-sm-auto.left-builder-col.builder-left-shrunk > div.componets-and-library-accordion.toolbox-card > div.card.mt-1 > div:nth-child(5) > div:nth-child(2)';
+    this.ActionDropdown='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > select';
+    this.ActionLabel='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table > tbody > tr:nth-child(2) > td > input';
+    this.ActionStyledropdown='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > select';
+    this.Actionsizedropdown='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table > tbody > tr:nth-child(4) > td:nth-child(2) > select';
+    this.ActionHoverTitle='#saltcorn-builder > div.row > div.col-sm-auto.builder-sidebar > div > div.settings-panel.card.mt-1 > div.card-body.p-2 > div > table > tbody > tr:nth-child(6) > td:nth-child(2) > input';
+
   }
 
   getRandomString(length) {
@@ -72,6 +92,8 @@ class PageObject {
     ]);
   }
 
+
+
   async dragAndDrop(source, target) {
     await this.page.locator(source).dragTo(this.page.locator(target));
   }
@@ -105,6 +127,7 @@ class PageObject {
     await this.page.waitForSelector(this.submitButton);
     await this.page.click(this.submitButton);
   }
+  
 
   async waitForToasterMessage() {
     await this.page.waitForSelector(this.toasterSelector);
--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
+ 
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,6 +25,7 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
