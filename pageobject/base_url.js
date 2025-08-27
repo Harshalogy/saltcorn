3916b0c9204115840bb3341cc5f1ca60fe686cf9
sumit mishra <sumit_mishra@live.in>
2025-08-27 23:03:59 +0530

    Upload_files_script

--- pageobject/base_url.js
+++ pageobject/base_url.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://e2etest.saltcorn.com';
+const baseURL = 'https://e2etest.saltcorn.co';
 //const baseURL = 'https://sumittestfeb07.saltcorn.com'
 const derivedURL = '/';
 const addemployee = '/view/Edit_Employee';
