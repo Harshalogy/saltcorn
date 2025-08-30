25115ff50c85c65dc40b48915ce2fadb72eeb905
sumit mishra <sumit_mishra@live.in>
2025-08-30 17:37:43 +0530

    fixing issues

--- playwright.config.js
+++ playwright.config.js
@@ -18,15 +18,14 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 0,  // Disable retries
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1, // Set the number of workers to 1
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  timeout:60000,
   use: {
-    headless: false, // or true if you want to run in headless mode
+    //headless: false, // or true if you want to run in headless mode
     
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
@@ -40,8 +39,7 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
+    }
     // ,
 
     // {
